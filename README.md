672200fadac2d4f99d1ec5074526dfd268380a8e
Thomas Dupré <duprethomasfr@gmail.com>
2020-04-20 00:12:02 +0200

    minor syntax fixes

---
+++ README.md
@@ -0,0 +1,1 @@
+shortcutifier.subscribe([65, 90], () => console.log('A & Z were pressed at the same time'))
--- src/index.js
+++ src/index.js
@@ -2,7 +2,4 @@ import SubscriptionHandler from './SubscriptionHandler';
 
 const shortcutifier = new SubscriptionHandler();
 
-const unsub1 = shortcutifier.subscribe([17, 65], () => console.log('yeet callback1'))
-const unsub2 = shortcutifier.subscribe([17, 90], () => console.log('yeet callback2'))
-
 export default shortcutifier;
